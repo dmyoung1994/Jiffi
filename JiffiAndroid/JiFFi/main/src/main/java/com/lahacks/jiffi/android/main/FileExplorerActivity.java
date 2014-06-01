4ad34c9611bd6fc9262d8db97989afb2062aae85
StanleyFung <stanleyfung94@hotmail.com>
2014-05-31 20:35:51 -0400

    File Explorer now grabs all PDF files on the phone and opens them with basic pdf viewer

--- JiffiAndroid/JiFFi/main/src/main/java/com/lahacks/jiffi/android/main/FileExplorerActivity.java
+++ JiffiAndroid/JiFFi/main/src/main/java/com/lahacks/jiffi/android/main/FileExplorerActivity.java
@@ -1,6 +1,5 @@
 package com.lahacks.jiffi.android.main;
 
-import android.app.AlertDialog;
 import android.app.ListActivity;
 import android.app.ProgressDialog;
 import android.content.Context;
@@ -9,15 +8,15 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Environment;
 import android.util.Log;
-import android.util.Pair;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.Spinner;
+import android.widget.TextView;
 
 import java.io.File;
-import java.util.Hashtable;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -26,19 +25,31 @@ import java.util.List;
  */
 public class FileExplorerActivity extends ListActivity {
     private static final String TAG = FileExplorerActivity.class.getCanonicalName();
-    File node;
-    List< String> pdfNames;
-    List<String> pdfPaths;
+    File dataDirectory, downloadDirectory, externalStorage;
+    ArrayList<String> pdfNames;
+    ArrayList<String> pdfPaths;
     ProgressDialog dialog;
+    TextView status;
     Context c;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        c=this;
-        node = Environment.getRootDirectory();
-        setUpLoadingSpinner(this);
+        setContentView(R.layout.fileexplorer);
+        c = this;
+        dataDirectory = Environment.getDataDirectory();
+        downloadDirectory = Environment.getDownloadCacheDirectory();
+        pdfNames = new ArrayList<String>();
+        pdfPaths = new ArrayList<String>();
+        String extState = Environment.getExternalStorageState();
+        //you may also want to add (...|| Environment.MEDIA_MOUNTED_READ_ONLY)
+//if you are only interested in reading the filesystem
+        if (extState.equals(Environment.MEDIA_MOUNTED)) {
+            externalStorage = Environment.getExternalStorageDirectory();
+        }
+        status = (TextView) findViewById(R.id.tv_currentPath);
         FindAllPDF task = new FindAllPDF();
-        task.execute(node);
+        task.execute();
     }
 
     @Override
@@ -49,32 +60,59 @@ public class FileExplorerActivity extends ListActivity {
         startActivity(intent);
     }
 
-    private class FindAllPDF extends AsyncTask<File, Boolean, Boolean> {
+    private class FindAllPDF extends AsyncTask<Void, Boolean, Boolean> {
+
 
         @Override
-        protected Boolean doInBackground(File... params) {
-            getPDFPaths(node);
+        protected Boolean doInBackground(Void... params) {
+
+            if (dataDirectory != null) {
+                getPDFPaths(dataDirectory);
+            }
+            if (downloadDirectory != null) {
+                getPDFPaths(downloadDirectory);
+            }
+            if (externalStorage != null) {
+                getPDFPaths(externalStorage);
+            }
             return true;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            setUpLoadingSpinner(c);
+            dataDirectory = Environment.getDataDirectory();
+            downloadDirectory = Environment.getDownloadCacheDirectory();
+            externalStorage = Environment.getExternalStorageDirectory();
         }
 
         @Override
         protected void onPostExecute(Boolean b) {
             super.onPostExecute(b);
             dialog.dismiss();
-            ArrayAdapter adapter = new ArrayAdapter(c,R.layout.fileexploreritem,R.id.tv_file_item,pdfNames);
-            setListAdapter(adapter);
+            if (pdfNames != null && pdfNames.size() > 0) {
+                ArrayAdapter adapter = new ArrayAdapter(c, R.layout.fileexploreritem, R.id.tv_file_item, pdfNames);
+                setListAdapter(adapter);
+            } else {
+                status.setText("You have no PDF files to read!");
+            }
         }
     }
 
     public void getPDFPaths(File node) {
-        Log.d(TAG, "Visiting: " + node.getAbsolutePath());
-        if (node.canRead() && node.getName().contains(".pdf")) {
-            Log.d(TAG, "PDF Found: " + node.getName());
-            pdfNames.add(node.getName());
-            pdfPaths.add(node.getAbsolutePath());
-        }
-        for (File f : node.listFiles()) {
-            getPDFPaths(f);
+        if (node != null) {
+            Log.d(TAG, "Visiting: " + node.getAbsolutePath());
+            if (node.canRead() && node.getName().contains(".pdf")) {
+                Log.d(TAG, "PDF Found: " + node.getName());
+                pdfNames.add(node.getName());
+                pdfPaths.add(node.getAbsolutePath());
+            }
+            if (node.listFiles() != null) {
+                for (File f : node.listFiles()) {
+                    getPDFPaths(f);
+                }
+            }
         }
     }
 
